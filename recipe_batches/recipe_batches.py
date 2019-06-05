57a2cd8096042eef9cac84fe2287a70cf6aa403c
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-06-05 16:28:37 -0400

    add recipe_batches solution

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,38 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  # find the limiting ingredient for a given recipe
+
+  max_possible = None
+
+  # keep track of ingredients we've used
+  recipe_keys = list(recipe.keys())
+  ingredient_keys = list(ingredients.keys())
+
+  # if we have keys in recipe that aren't in ingredients,
+  # we can't make anything
+  if len(list(set(recipe_keys) - set(ingredient_keys))) > 0:
+    return 0
+
+  # iterate over dictionary
+  for k,v in ingredients.items():
+
+    # skip if recipe doesn't have this ingredient
+    if not k in recipe:
+      continue
+
+    num_possible = v // recipe[k]
+    if max_possible is None or num_possible < max_possible:
+      max_possible = num_possible
+
+  return max_possible
+
+
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+  ingredients = { 'milk': 132, 'butter': 51, 'flour': 51 }
   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
